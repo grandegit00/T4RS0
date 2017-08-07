630bf1cbe9bc382d2cea52654d585c5f8110c192
Jose Antonio Delgado <jadelgado@us.es>
2017-08-07 15:25:05 +0200

    [ADD] - Git init, 1st commit

---
+++ config_port.c
@@ -0,0 +1,20 @@
+TRISB = 0xff;  // Configure PORTB as Input.
+ 
+TRISC = 0x00; // Configure PORTC as Output.
+ 
+TRISD = 0x0F; // Configure lower nibble of PORTD as Input and higher nibble as Output
+ 
+TRISD = (1<<0) | (1<<3) | (1<<6); // Configure PD0,PD3,PD6 as Input and others as Output
+                                  // 
+                                  // PORTB = 0xff;  // Make all PORTB pins HIGH.
+ 
+PORTC = 0x00;  // Make all PORTC pins LOW..
+ 
+PORTD = 0x0F;  // Make lower nibble of PORTD as HIGH and higher nibble as LOW 
+ 
+PORTD = (1<<PD0) | (1<<PD3) | (1<<PD6); // Make PD0,PD3,PD6 HIGH,
+ 
+ 
+TRISB = 0x00;  // Configure PORTB as Output
+TRISD = 0xff;  // Configure PORTD as Input
+PORTD = PORTB; // Read the data from PORTB and send it to PORTD.
